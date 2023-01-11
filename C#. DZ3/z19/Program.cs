8bc13317d3cebb03060633011d22a380166637f3
Cunarepa <119619575+Cunarepa@users.noreply.github.com>
2023-01-12 01:49:27 +0300

    Задача 19

---
+++ C#. DZ3/z19/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число N ");
+int N = Convert.ToInt32(Console.ReadLine());
+int secondDigit = N/1000;
+int fourthDigit = N/10;
+Console.WriteLine(N);
+if (N<=9999 || N>99999)
+{
+    Console.WriteLine("Введённое число не является пятизначным");
+}
+else if ((N-N%10000)/10000 == N%10 && (secondDigit%10 == fourthDigit%10))
+{
+    Console.WriteLine("является полиндромом");
+}
+else if ((N-N%10000 != N%10) && (secondDigit%10 != fourthDigit%10))
+{
+    Console.WriteLine("не является полиндромом");
+}

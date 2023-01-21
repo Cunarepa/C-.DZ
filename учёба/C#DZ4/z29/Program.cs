a9510f561801a043533243d4ae005bfeca0d4ef7
Cunarepa <119619575+Cunarepa@users.noreply.github.com>
2023-01-21 10:47:40 +0300

    Задача 29

---
+++ учёба/C#DZ4/z29/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int[]array = GetRandomArray(8,1,40);
+Console.WriteLine($"[{String.Join("|", array)}]");
+
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int [size];
+    for (int i=0; i<size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return result;
+}

609ca86b3b6c12f3caeff152d1e899cb16b33771
Cunarepa <119619575+Cunarepa@users.noreply.github.com>
2023-01-21 10:04:28 +0300

    Задача 25

---
+++ учёба/C#DZ4/z25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ($"{A} в степени {B} = {Pow(A,B)}");
+
+
+int Pow(int num, int rank)
+{
+    if(rank==0) return 1;
+    int result = num;
+    for (int i=2; i<=rank; i++)
+    {
+        result *= num;
+    }
+    return result;
+}

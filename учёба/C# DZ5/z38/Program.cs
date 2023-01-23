f49369ca066aedd6302c3787f9373eb7720ac753
Cunarepa <119619575+Cunarepa@users.noreply.github.com>
2023-01-23 12:55:42 +0300

    Задача 38

---
+++ учёба/C# DZ5/z38/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+int[] startArray = GetArray(5, 0, 100);
+Console.WriteLine(String.Join(" ", startArray));
+Console.WriteLine(GetDifference(startArray));
+
+int[]GetArray(int size, int minValue, int maxValue)
+{
+    int[]res = new int [size];
+    for (int i = 0; i<size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return res;
+}
+
+int GetDifference(int[]array)
+{
+    int min = array[0];
+    int max = array[0];
+    foreach(var item in array)
+    {
+        if(min>item)min = item;
+        if(max<item)max = item;
+    }
+    return max - min;
+}

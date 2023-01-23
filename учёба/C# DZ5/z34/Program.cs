c7aea3fdbebf87e9f7da8798e98de119b059d343
Cunarepa <119619575+Cunarepa@users.noreply.github.com>
2023-01-23 09:16:16 +0300

    Задача 34

---
+++ учёба/C# DZ5/z34/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+int[]startArray = GetArray(4, 100, 999);
+Console.WriteLine(String.Join(" ", startArray));
+Console.WriteLine(GetEvenCount(startArray));
+
+int[]GetArray(int size,int minValue, int maxValue)
+{
+    int[]res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return res;
+}
+
+int GetEvenCount(int[]array)
+{
+    int count = 0;
+    foreach(var item in array)
+    {
+        if(item%2==0) count++;
+    }
+    return count;
+}

8d463be4e97e057a3a64c87b64e1d6bd6d7bc1f3
Cunarepa <119619575+Cunarepa@users.noreply.github.com>
2023-01-23 09:36:46 +0300

    Задача 36

---
+++ учёба/C# DZ5/z36/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+int[]startArray = GetArray(4, -100, 100);
+Console.WriteLine(String.Join(" ", startArray));
+Console.WriteLine(GetSumOdd(startArray));
+
+int[]GetArray(int size, int minValue, int maxValue)
+{
+    int[]res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return res;
+}
+
+int GetSumOdd(int[] array)
+{
+    int sum = 0;
+    for (int i=0; i < array.Length; i++)
+    {
+        if(i%2==1) sum+=array[i];
+    }
+    return sum;
+}

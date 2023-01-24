c80c11019a7e8b19fd4ca414999ae096fd7862e1
Cunarepa <119619575+Cunarepa@users.noreply.github.com>
2023-01-24 09:18:09 +0300

    Задача 41

---
+++ учёба/C# DZ6/z41/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+Console.Write("Введите числа через пробел: ");
+int[]numbers = GetArrayFromString(Console.ReadLine());
+Console.WriteLine ($"Количество чисел больше 0: {GetCountPositiveElements(numbers)}");
+
+
+int[]GetArrayFromString(string stringArray)
+{
+    string[]nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[]result = new int [nums.Length];
+    for(int i = 0; i<result.Length; i++)
+    {
+        result[i]= int.Parse(nums[i]);
+    }
+    return result;
+}
+
+
+int GetCountPositiveElements(int[]array)
+{
+    int count = 0;
+    foreach (int item in array)
+    {
+        if (item > 0)  count++;
+    }
+    return count;
+}
+        

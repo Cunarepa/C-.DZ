31a016c39fafce94f9926e1a56f9e4680e85c80a
Cunarepa <119619575+Cunarepa@users.noreply.github.com>
2023-01-25 01:05:50 +0300

    Задача 43

---
+++ учёба/C# DZ6/z43/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите через пробел значения b1, k1, b2, k2: ");
+string[]f = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+double b1 = double.Parse(f[0]);
+double k1 = double.Parse(f[1]);
+double b2 = double.Parse(f[2]);
+double k2 = double.Parse(f[3]);
+
+Console.WriteLine(String.Join(" ", GetPoint(b1, k1, b2, k2)));
+
+double[] GetPoint (double onB1, double onK1, double onB2, double onK2)
+{
+    double[]result = new double[2];
+    result[0] = (onB2 - onB1)/(onK1 - onK2);
+    result[1] = onK1 * result[0] + onB1;
+    return result;
+}

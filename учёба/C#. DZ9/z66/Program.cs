69aeaead4f6d314488d20076a81ec7141a20cfe4
Cunarepa <119619575+Cunarepa@users.noreply.github.com>
2023-02-15 23:20:39 +0300

    Задача 66

---
+++ учёба/C#. DZ9/z66/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.WriteLine("Задайте значение M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте значение N: ");
+int n = int.Parse(Console.ReadLine());
+
+int SumNutEl(int firstNum, int secondNum)
+{
+
+    int sum = 0;
+    if (firstNum == secondNum) return secondNum;
+    sum = firstNum + SumNutEl(firstNum + 1, secondNum);
+    return sum;
+}
+
+Console.WriteLine(SumNutEl(m, n));

02c0878015bc37f5a3f06d85549dfc9bb36e9542
Cunarepa <119619575+Cunarepa@users.noreply.github.com>
2023-02-15 22:58:29 +0300

    Задача 64

---
+++ учёба/C#. DZ9/z64/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Задайте значение N: ");
+int N = int.Parse(Console.ReadLine());
+
+void PrintNumbers(int start)
+{
+    if (start < 1) return;
+    Console.Write($"{start} ");
+    if (start == 1) return;
+    PrintNumbers(start - 1);
+}
+
+PrintNumbers(N);

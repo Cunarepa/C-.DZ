a1d8c1f028a605ee7d920d4bfd8a2191edab6d9b
Cunarepa <119619575+Cunarepa@users.noreply.github.com>
2023-02-15 23:26:54 +0300

    Задача 68

---
+++ учёба/C#. DZ9/z68/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите M: ");
+int mA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите N: ");
+int nA = int.Parse(Console.ReadLine());
+
+int Ackermann(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Ackermann(m - 1, 1);
+    else return Ackermann(m - 1, Ackermann(m, n - 1));
+}
+
+Console.WriteLine(Ackermann(mA, nA));

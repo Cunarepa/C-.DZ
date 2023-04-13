0f0af21f326ea4907144f1a1c75af411d08da484
Cunarepa <119619575+Cunarepa@users.noreply.github.com>
2023-04-13 22:48:57 +0300

    Задача 2

---
+++ учёба/PYTHON/main.py
@@ -0,0 +1,65 @@
+#print(1)
+
+#value=None
+#a = 123
+#b = 1.23
+#print(a)
+#print(b)
+#value = 1234
+#print(value)
+
+#print (type(b))
+
+# s = 'hello,'
+# w = "world"
+# print(s, w)
+
+# s = 'hello "world'
+# print(s)
+
+# a = 3
+# b = 11
+# s = 2022
+# print(a, b, s)
+
+# print('{} - {} - {}' .format(a,b,s))
+# print(f'first - {a} second - {b} third - {s}')
+
+# n = 1.345
+# print(int(n))
+
+# m = '345'
+# print(m * 2)
+
+# print(int(m) * 2)
+# print(str(n) * 2)
+
+# n = '1.345'
+# print(float(n) * 2)
+# m = 2
+# print (float(m))
+
+n = int(input('Введите трёхзначное число: '))
+print(n%10 + n//100 + n//10%10) 
+
+# s = int(input('Введите общее количество журавликов: '))
+# m = k = s//6
+# n = 2 * (s//6 + s//6)
+# print (m, n, k)
+
+# b = int (input ("Введите номер билетика: "))
+# m = b//1000
+# n = b%1000
+# if m//100 + m//10%10 + m%10 == n//100 + n//10%10 + n%10:
+#     print ('yes')
+# else:
+#     print ('no')    
+
+
+# m = int(input('Введите кол-во долек в длину: '))
+# n = int(input('Веедите кол-во долек в ширину: '))
+# k = int(input('Введите кол-во долек, которые нужно отломить: '))
+# if k%m == 0 or k%n == 0:
+#     print('yes')
+# else:
+#     print('no')    

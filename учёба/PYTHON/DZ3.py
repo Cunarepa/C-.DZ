6718ec0a807770ee9e83cd60c483e5725cee015c
Cunarepa <119619575+Cunarepa@users.noreply.github.com>
2023-05-05 17:57:53 +0300

    Задача 18

--- учёба/PYTHON/DZ3.py
+++ учёба/PYTHON/DZ3.py
@@ -1,6 +1,20 @@
-A = []
-N = int(input('Вводим количество элементов в массиве: '))
+# A = []
+# N = int(input('Вводим количество элементов в массиве: '))
+# for i in range (N):
+#     A.append(int(input()))
+# X = int(input("Искомое число: "))
+# print (A.count(X))    
+
+N = int(input("Введите количество элементов в массиве: "))
+list = []
 for i in range (N):
-    A.append(int(input()))
-X = int(input("Искомое число: "))
-print (A.count(X))    
+    list.append(int(input()))
+X = int(input("Введите заданное число: "))
+diff_min = abs(list[0] - X)
+k = list[0]
+for i in list:
+    if abs(i - X) < diff_min:
+        diff_min = abs(i - X)
+        k = i
+print(k)        
+    
